0ae328afea02894d6ad974e1e78a2dab9e383657
Brunswick-Khorasan <quixoticbibliomania@gmail.com>
2025-09-15 12:15:06 -0500

    adding updater for the header

---
+++ updater.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+# Updates all html files' headers to the header in basefile
+def update(*filenames,basefile='base.html'):
+    print(filenames)
+    if len(filenames) == 0:
+        filenames = [f for f in os.listdir() if f.endswith('.html') and f != 'base.html']
+
+    newheader = ''
+    print('Reading',basefile)
+    with open(basefile,'r') as basefile:
+        newheader = re.search(r'<header>.*<\/header>',basefile.read(),flags=re.DOTALL)[0]
+    print(newheader)
+
+    for filename in filenames:
+        print('Reading',filename)
+        filetext = ''
+        with open(filename,'r') as file:
+            filetext = file.read()
+        print('Replacing header')
+        filetext = re.sub(r'<header>.*<\/header>',newheader,filetext,flags=re.DOTALL)
+        print(filetext)
+
+update()
